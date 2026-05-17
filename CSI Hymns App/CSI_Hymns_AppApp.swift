2c87fa2236c094a54442a564bc3d1e08122dd593
Reynold29 <reynoldclare02@gmail.com>
2026-05-17 13:17:56 +0530

    Initial Commit

---
+++ CSI Hymns App/CSI_Hymns_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSI_Hymns_AppApp.swift
+//  CSI Hymns App
+//
+//  Created by Reynold Clare on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CSI_Hymns_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSI Hymns App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSI Hymns App
+//
+//  Created by Reynold Clare on 17/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
